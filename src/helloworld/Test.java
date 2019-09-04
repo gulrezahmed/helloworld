6830f6c5e51f164ad5c77743b4712cbe9dd52ff9
KH0338 <KH0338@NLVHPRAABW30843.solon.prd>
2019-09-04 13:55:45 +0200

    Added test class

---
+++ src/helloworld/Test.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package helloworld;
+
+/**
+ * @author KH0338
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
